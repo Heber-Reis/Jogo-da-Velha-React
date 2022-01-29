0e407e11fcb6872df317e9de9e4159982a9a2916
Heber Magno <heber2014gbi@gmail.com>
2022-01-29 20:54:44 -0300

    criado header

---
+++ src/components/subtitle/subtitle.jsx
@@ -0,0 +1,11 @@
+import styles from "./style.module.css";  
+
+function Subtitle ({children}){
+  return(
+    <>
+      <h6 className={styles.subtitle}>{children}</h6>
+    </>
+  )
+}
+
+export default Subtitle;
