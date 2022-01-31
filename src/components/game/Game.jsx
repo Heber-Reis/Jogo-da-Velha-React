5d99ccc008032407a86e684a73cb172d211024d5
Heber Magno <heber2014gbi@gmail.com>
2022-01-31 16:06:02 -0300

    criado estados GameOption

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -1,19 +1,24 @@
+import { useState } from "react";
+
 import styles from "./style.module.css";
 
 import GameOption from "../gameoption/GameOption";
 
 function Game () {
+
+  const [gameState, setGameState] = useState(Array(9).fill(1)) //Array(num).fill(num) cria um array com n posições e com valores iniciais de n
+
+  console.log(gameState);
   return(
     <div className={styles.game}>
-      <GameOption/>
-      <GameOption/>
-      <GameOption/>
-      <GameOption/>
-      <GameOption/>
-      <GameOption/>
-      <GameOption/>
-      <GameOption/>
-      <GameOption/>
+      {
+        gameState.map((value, position) =>
+          <GameOption
+            key={`game-option-pos-${position}`}//o map no react precisa de uma key unica nos componentes
+            status={value}
+          />
+        )
+      }
     </div>
   )
 }
--- src/components/gameoption/GameOption.jsx
+++ src/components/gameoption/GameOption.jsx
@@ -1,9 +1,17 @@
 import styles from "./style.module.css";
+import Icon from "../icon/icon"
 
-function GameOption () {
+const GameIcon = ({ iconName }) => <Icon iconName={iconName} size="25px"/>//dessa forma não preciso repetir o size
+
+function GameOption ({ status }) {
   return(
     <div className={styles.gameOption}>
-      ]
+      {
+        status === 1 && <GameIcon iconName="circle"/>
+      }      
+      {
+        status === -1 && <GameIcon iconName="x"/>
+      }
     </div>
   )
 }
