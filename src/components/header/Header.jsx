c018d9fa6303630b102a8d0f43bd620ead8011da
Heber Magno <heber2014gbi@gmail.com>
2022-01-30 21:22:56 -0300

    criado componente icone

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,12 +2,16 @@ import styles from "./style.module.css";
 
 import Title from "../title/Title";
 import Subtitle from "../subtitle/subtitle";
+import Icon from "../icon/icon";
 
 function Header () {
   return(
     <div className={styles.header}>
       <Title>Jogo da Velha</Title>
       <Subtitle>Criado por Heber Magno</Subtitle>
+      <div className={styles.iconContent}>
+        <Icon iconName={'github'} link={"https://github.com"}/>
+      </div>
     </div>
   )
 }
